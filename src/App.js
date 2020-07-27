2a58957b19b203370d06697f3a41171d576b9e41
Myeong geun <zl6575@naver.com>
2020-07-27 19:09:14 +0900

    Learn class react component and setState concept

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 
-function Food({ fav }) {
-  console.log({ fav });
-  return <h1>I like Potato</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav="Kimchi" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
