3c2fee2031c520e5edf6f1935d15339c91653e08
Myeong geun <zl6575@naver.com>
2020-08-01 16:49:48 +0900

    Axios data(fetching data with axios) from yts API

--- README.md
+++ README.md
@@ -27,3 +27,35 @@ React JS fundamentals Course (2019 Update!)
 3. unmounting 컴포넌트(component)가 죽는 것을 의미함  
    그게 컴포넌트가 돔에서 제거 될 때를 의미  
    componentWillUnmount()
+
+### setTimeout 이란?
+
+#### js 시간 지연 함수, 일정 시간 뒤 실행시키는 것.
+
+#### 코드작성 방법
+
+#### setTimeout(function() { // Code here }, delay);
+
+### data를 가져오는 것 : fetch
+
+#### 하지만 니꼬는 fetch 대신 axios 가 더 좋은 방법이라고 한다.
+
+#### axios로 데이터를 가져올 것임
+
+#### npm i axios 하고 import axios
+
+### yts API 사용하기.
+
+#### https://yts.mx/api/v2/list_movies.json
+
+#### 니꼬의 프록시 url https://yts.mx/api/v2/list_movies.json
+
+#### {data: {data: { movies },},} 이것은 data.data.movies와 같다.
+
+#### 함수가 끝나고 나는 걸 확인하려고 async await를 사용함.
+
+### 코딩이 어떻게 일어나는가
+
+데이터 가져옴(axios 함).
+그것을 setState로 상태에 있는 영화 배열에 추가하였고,
+데이터를 가져오는 것이 완료되면 로딩이 완료 준비됨이라는 표시로 만듬.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from "react";
+import axios from "axios";
 //import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    //데이터를 가져오기 위한 것. 영화 데이터가 존재함. get 함수는 시간이 오래 걸림.
+    // 그래서 자바스크립트에게 this 함수가 끝날 때까지 시간이 걸릴 수 있다는 걸 인지 시켜줘야함.
+    //async는 비동기 await 한 것을 기다려라.
+    this.setState({ movies, isLoading: false });
+    // state 의 상태를 변경할 것임. state.movies 배열에 가져온 movies 데이터 넣을 것 + isLoading 값 변경
   };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  // {/* 함수 만들고, delay*/}
   render() {
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "we are ready"}</div>;
   }
 }
 
